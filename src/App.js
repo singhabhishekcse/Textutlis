f08ce78b2c350b6ac30596278b8af9718142d3b3
singhabhishekcse <singhabhishekcse@yahoo.com>
2021-09-03 11:49:49 +0530

    creating a textulis project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
+// import About from './Components/about.js';
+// import Card from './Components/card';
+import NavBar from './Components/NavBar';
+import TextForm from './Components/TextForm';
 
 function App() {
+   const [mode, setMode] = useState("light")
+  
+   const toggleMode = ()=>{
+    if(mode === "light"){
+      setMode("dark")
+      document.body.style.backgroundColor = "black"
+    }else{
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+    }
+
+  }
+  
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <NavBar mode={mode} toggleMode = {toggleMode}  title="Textutlis" home="Home" blog="Blog" about="About"  login="Log In" logout="Log Out" />
+
+       <TextForm heading="Enter Your Text Below" mode={mode} />
+
+        {/* <Card />
+       <About /> */}
+        
+
+
+    </>
   );
 }
 
